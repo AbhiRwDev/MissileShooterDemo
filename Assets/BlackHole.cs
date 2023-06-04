a5ba6e7a035f8ea8a341495dff7ec965496c6637
anshurw <anshurw@gmail.com>
2023-06-04 12:59:19 +0530

    update

---
+++ Assets/BlackHole.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHole : MonoBehaviour
+{
+    public float Duration;
+    public float ShrinkSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Duration>=0)
+        {
+            Duration -= Time.deltaTime;
+        }
+        else
+        {
+            transform.localScale = Vector3.Lerp(transform.lossyScale,Vector3.zero,Time.deltaTime*ShrinkSpeed);
+        }
+    }
+    
+}
